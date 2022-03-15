1e579bed654b15497cf713b491ef42260015ecaa
Sean Williams <sean.williams@derivco.co.uk>
2022-03-15 19:11:24 +0000

    blah

--- config/test.exs
+++ config/test.exs
@@ -16,6 +16,16 @@ config :jobs, Jobs.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, App.Repo,
+    username: "postgres",
+    password: "postgres",
+    hostname: "localhost",
+    database: "jobs_test"
+end
+
+
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :jobs, JobsWeb.Endpoint,
