8bc7adaf976908b11694a0fafb3281c07edbc5e3
Sean Williams <sean.williams@derivco.co.uk>
2022-03-15 19:17:39 +0000

    blah

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 
-IO.puts "=========> #{System.get_env('GITHUB_ACTIONS')}"
+IO.puts "=========> #{System.get_env("GITHUB_ACTIONS")}"
 
 
 config :jobs, Jobs.Repo,
